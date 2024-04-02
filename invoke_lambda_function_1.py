c2ec6dea3b7955593a5aa1669bb6789f88cd447d
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-04-01 23:59:48 -0400

    Update invoke_lambda_function_1.py

--- invoke_lambda_function_1.py
+++ invoke_lambda_function_1.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
+
 import boto3
 
 param1 = os.environ["PARAM1"]
@@ -9,3 +11,28 @@ param2 = os.environ["PARAM2"]
 print("Invoke Lambda Function with the following parameters:")
 print(f"{param1 = }")
 print(f"{param2 = }")
+
+
+def main():
+    lbd_client = boto3.client("lambda")
+    # ref: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lambda/client/invoke.html
+    payload = {
+        "param1": param1,
+        "param2": param2,
+    }
+    res = lbd_client.invoke(
+        FunctionName="your_lambda_function_name",
+        InvocationType="RequestResponse",
+        LogType="Tail",
+        Payload=json.dumps(payload),
+    )
+    print("--- log ---")
+    log = res["LogResult"]
+    print(log)
+    print("--- output ---")
+    output = res["Payload"].read().decode("utf-8")
+    print(output)
+
+
+# if __name__ == "__main__":
+#     main()
