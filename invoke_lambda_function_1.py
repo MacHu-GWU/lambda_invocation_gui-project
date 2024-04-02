4bcf05db40c0386767d54bc01ed80e50e8857045
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-04-01 23:37:08 -0400

    c

--- invoke_lambda_function_1.py
+++ invoke_lambda_function_1.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+import boto3
 
 param1 = os.environ["PARAM1"]
 param2 = os.environ["PARAM2"]
+
+print("Invoke Lambda Function with the following parameters:")
 print(f"{param1 = }")
 print(f"{param2 = }")
